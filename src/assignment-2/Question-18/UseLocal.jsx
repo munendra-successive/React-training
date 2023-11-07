8593e24cbb800e4f1659c0452b80b48b5e36cd56
munendra-successive <munendra.kushwaha@successive.tech>
2023-11-07 14:40:39 +0530

    modified code to clear input fields in question 19

--- src/assignment-2/Question-18/UseLocal.jsx
+++ src/assignment-2/Question-18/UseLocal.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import useLocalStorage from "./useLocalStorage";
 
@@ -13,12 +12,29 @@ Develop a component that uses the useLocalStorage hook to manage user preference
 */
 
 const UseLocal = () => {
-  const [key, setKey] = useState("")
+  const [key, setKey] = useState("");
   const [value, setValue1] = useState("");
+  const [receiveKey, setReceiveKey] = useState("");
+  const [deleteKey, setDeleteKey] = useState("");
   const [receivedValue, setReceivedValue] = useState("");
   const [getValue, setValue, removeValue] = useLocalStorage();
-  const handler = (e) => {
-    setReceivedValue(getValue(key));
+  const handleClearInput = () => {
+    setKey("");
+    setValue1("");
+    setReceiveKey("");
+    setDeleteKey("");
+  };
+  const handleGet = () => {
+    setReceivedValue(getValue(receiveKey));
+    handleClearInput();
+  };
+  const handleDelete = () => {
+    removeValue(deleteKey);
+    handleClearInput();
+  };
+  const handleSet = () => {
+    setValue(key, value);
+    handleClearInput();
   };
 
   return (
@@ -34,44 +50,36 @@ const UseLocal = () => {
       <h4>Set Item in localStorage</h4>
       <input
         placeholder="Enter Key"
+        value={key}
         onChange={(e) => setKey(e.target.value)}
         type="text"
       />
       <input
         placeholder="Enter Value"
+        value={value}
         onChange={(e) => setValue1(e.target.value)}
         type="text"
       />
-      <button
-        onClick={() => {
-          setValue(key, value);
-        }}
-      >
-        Submit
-      </button>
+      <button onClick={handleSet}>Submit</button>
 
       <h4>Get item from localStorage</h4>
       <input
         placeholder="Enter Key"
-        onChange={(e) => setKey(e.target.value)}
+        value={receiveKey}
+        onChange={(e) => setReceiveKey(e.target.value)}
         type="text"
       />
-      <button onClick={handler}>Submit</button>
+      <button onClick={handleGet}>Submit</button>
       <h4>Value: {receivedValue}</h4>
 
       <h4>Delete Item from localStorage</h4>
       <input
         placeholder="Enter Key"
-        onChange={(e) => setKey(e.target.value)}
+        value={deleteKey}
+        onChange={(e) => setDeleteKey(e.target.value)}
         type="text"
       />
-      <button
-        onClick={() => {
-          removeValue(key);
-        }}
-      >
-        Submit
-      </button>
+      <button onClick={handleDelete}>Submit</button>
     </>
   );
 };

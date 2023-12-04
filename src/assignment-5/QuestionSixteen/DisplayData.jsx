629c0fc854402817aa5f29dda72756055d76ea6e
munendra-successive <munendra.kushwaha@successive.tech>
2023-12-04 18:59:49 +0530

    centred list

--- src/assignment-5/QuestionSixteen/DisplayData.jsx
+++ src/assignment-5/QuestionSixteen/DisplayData.jsx
@@ -6,18 +6,19 @@ const DisplayData = ({ data }) => {
         and passes it as props to a component. Use this HOC to retrieve and
         display data in a component.
       </h4>
+      <div style={{display:"flex",justifyContent:"center"}}>
       <ul
         style={{
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
+          textAlign:"left",
         }}
       >
         {data.map((item, index) => (
           <li key={index}>{item.name}</li>
         ))}
-      </ul>
+      </ul> 
+      </div>
     </>
   );
 };
